25bfa83130d3822f1b115f92ab107264658a4891
860230 <mpaushev2@mail.ru>
2020-11-07 05:56:53 +0300

    b - edited even.lua added

---
+++ lib/event.lua
@@ -0,0 +1,58 @@
+local event = {}
+
+local function createPlainFilter(name, ...)
+  local filter = table.pack(...)
+  if name == nil and filter.n == 0 then
+    return nil
+  end
+
+  return function(...)
+    local signal = table.pack(...)
+    if name and not (type(signal[1]) == "string" and signal[1]:match(name)) then
+      return false
+    end
+    for i = 1, filter.n do
+      if filter[i] ~= nil and filter[i] ~= signal[i + 1] then
+        return false
+      end
+    end
+    return true
+  end
+end
+
+
+function event.pull(...)
+  local args = table.pack(...)
+  if type(args[1]) == "string" then
+    return event.pullFiltered(createPlainFilter(...))
+  else
+    checkArg(1, args[1], "number", "nil")
+    checkArg(2, args[2], "string", "nil")
+    return event.pullFiltered(args[1], createPlainFilter(select(2, ...)))
+  end
+end
+
+function event.pullFiltered(...)
+  local args = table.pack(...)
+  local seconds, filter = math.huge
+
+  if type(args[1]) == "function" then
+    filter = args[1]
+  else
+    checkArg(1, args[1], "number", "nil")
+    checkArg(2, args[2], "function", "nil")
+    seconds = args[1]
+    filter = args[2]
+  end
+
+  repeat
+    local signal = table.pack(computer.pullSignal(seconds))
+    if signal.n > 0 then
+      if not (seconds or filter) or filter == nil or filter(table.unpack(signal, 1, signal.n)) then
+        return table.unpack(signal, 1, signal.n)
+      end
+    end
+  until signal.n == 0
+end
+
+return event
---
+++ lib/readme.md
@@ -0,0 +1,2 @@
+event.lua is strongly cut version of OpenOS event library
+for usage with https://github.com/cyntachs/LiskelOS
